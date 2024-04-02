7ffc8761ec9110381af87a49c73dfd3cb617e8bf
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-04-02 23:03:33 +0700

    update authen

--- src/main/java/com/minhdunk/research/controller/AuthenticationController.java
+++ src/main/java/com/minhdunk/research/controller/AuthenticationController.java
@@ -62,11 +62,13 @@ public class AuthenticationController {
         userOutputDTO.setToken(accessToken);
         log.info("User " + request.getUsername() + " login successfully");
 
-        if (user.getEnabled()){
-            return ResponseEntity.ok(userOutputDTO);
-        } else{
-            return ResponseEntity.status(409).body(userOutputDTO);
-        }
+//        if (user.getEnabled()){
+//            return ResponseEntity.ok(userOutputDTO);
+//        } else{
+//            return ResponseEntity.status(409).body(userOutputDTO);
+//        }
+
+        return ResponseEntity.ok(userOutputDTO);
 
     }
 
