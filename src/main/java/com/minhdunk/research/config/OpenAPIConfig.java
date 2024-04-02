a673ed00b385c48e7ace121443159719fd89f58d
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2024-04-03 00:04:00 +0700

    Update authen

--- src/main/java/com/minhdunk/research/config/OpenAPIConfig.java
+++ src/main/java/com/minhdunk/research/config/OpenAPIConfig.java
@@ -1,10 +1,14 @@
 package com.minhdunk.research.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,30 +25,39 @@ public class OpenAPIConfig {
     @Value("${bezkoder.openapi.prod-url}")
     private String prodUrl;
 
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
     @Bean
     public OpenAPI myOpenAPI() {
-        Server devServer = new Server();
-        devServer.setUrl(devUrl);
-        devServer.setDescription("Server URL in Development environment");
-
         Server prodServer = new Server();
         prodServer.setUrl(prodUrl);
         prodServer.setDescription("Server URL in Production environment");
 
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
         Contact contact = new Contact();
-        contact.setEmail("nguyendangminh49@gmail..com");
+        contact.setEmail("nguyendangminh49@gmail.com");
         contact.setName("Nguyen Dang Minh");
         contact.setUrl("https://www.facebook.com/minhdunk/");
 
         License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
         Info info = new Info()
-                .title("Quiz Management API by E01")
+                .title("Teaching Method Research API by MinhDunk")
                 .version("1.0")
                 .contact(contact)
                 .description("This API exposes endpoints to manage demo.")
                 .license(mitLicense);
 
-        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+        return new OpenAPI().info(info).servers(List.of(prodServer, devServer))
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
 }
